1481febf3bd7c859af8b845e14ea0a8c0224ee64
Suraj patil <surajpatil8208@gmail.com>
2024-04-25 10:49:33 +0530

    f1 change

--- CppVersionControl/alpha.h
+++ CppVersionControl/alpha.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <iostream>
 // adding new message in feature
-int main() {
-	std::cout << "alpha change for feature in main" << std::endl;
+
+int first(int num1 = 32) {
+	std::cout << "first" << num1 << std::endl;
+}
+int second(int num2 = 22) {
+	std::cout << "second" << num2 << std::endl;
+}
+
+int alpha() {
+	std::cout << "sum of num : " << first() + second() << std::endl;
 }
