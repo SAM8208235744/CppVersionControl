4e4f3252db6b2bff8e4766fc897c3b38575ad3de
Suraj patil <surajpatil8208@gmail.com>
2024-04-25 10:49:55 +0530

    f2 change

--- CppVersionControl/alpha.h
+++ CppVersionControl/alpha.h
@@ -2,10 +2,10 @@
 #include <iostream>
 // adding new message in feature
 
-int first(int num1 = 32) {
+int first(int num1 = 22) {
 	std::cout << "first" << num1 << std::endl;
 }
-int second(int num2 = 22) {
+int second(int num2 = 32) {
 	std::cout << "second" << num2 << std::endl;
 }
 
